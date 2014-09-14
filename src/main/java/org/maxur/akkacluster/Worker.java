7e7b72168b149dfd49bc8fe1e1aa73f931be9df0
Maxim Yunusov <maximyunusov@gmail.com>
2014-09-14 15:07:07 +0700

    Снимаем проблему конкурентного доступа (используем Akka)

--- src/main/java/org/maxur/akkacluster/Worker.java
+++ src/main/java/org/maxur/akkacluster/Worker.java
@@ -1,12 +1,21 @@
 package org.maxur.akkacluster;
 
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import static akka.actor.ActorRef.noSender;
 import static java.lang.String.format;
 
 /**
  * @author Maxim Yunusov
  * @version 1.0 14.09.2014
  */
-public class Worker {
+public class Worker extends UntypedActor {
+
+    private LoggingAdapter logger = Logging.getLogger(context().system(), this);
 
     private int count = 0;
 
@@ -14,24 +23,33 @@ public class Worker {
 
     private Repository repository;
 
-    public Worker() {
-        init();
+    public static void main(String[] args) throws Exception {
+        ActorSystem system = ActorSystem.create("WorkerSystem");
+        system.actorOf(Props.create(Worker.class));
     }
 
-    public void init() {
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof String) {
+            final String response = format("%d: %s", count++, message);
+            repository.save(response);
+            sender.send(response);
+            sender().tell(response, noSender());
+        }
+    }
+
+    @Override
+    public void preStart() throws Exception {
+        logger.info("Start Worker");
         sender = new Sender();
         repository = new Repository();
     }
 
-    public synchronized String run(String request) throws Exception {
-        final String message = format("%d: %s", count++, request);
-        repository.save(message);
-        sender.send(message);
-        return message;
-    }
-
-    public void done() {
+    @Override
+    public void postStop() throws Exception {
         sender.done();
         repository.done();
+        logger.info("Stop Worker");
     }
+
 }
