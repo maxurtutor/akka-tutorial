0b676ca5192e913676bb06c4eb19145f73cd9b6b
Maxim Yunusov <maximyunusov@gmail.com>
2014-09-14 14:25:38 +0700

    Добавляем клиента

--- src/main/java/org/maxur/akkacluster/Client.java
+++ src/main/java/org/maxur/akkacluster/Client.java
@@ -2,19 +2,25 @@ package org.maxur.akkacluster;
 
 
 import org.perf4j.log4j.Log4JStopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Maxim Yunusov
  * @version 1.0 14.09.2014
  */
-public class Client {
+public class Client implements Runnable {
+
+    Logger logger = LoggerFactory.getLogger(Client.class);
+
 
     private Worker worker;
 
     public static void main(String[] args) throws Exception {
         final Client client = new Client();
         client.init();
-        client.run();
+        new Thread(client).start();
+        new Thread(client).start();
         client.done();
     }
 
@@ -22,15 +28,20 @@ public class Client {
         worker = new Worker();
     }
 
-    private void run() throws Exception {
-        for (int i = 0; i < 100; i++) {
-            Log4JStopWatch stopWatch = new Log4JStopWatch("Client");
+    public void run() {
+        try {
+            for (int i = 0; i < 100; i++) {
+                Log4JStopWatch stopWatch = new Log4JStopWatch("Client");
 
-            String response = worker.run(makeRequest());
-            System.out.println(response);
+                String response = worker.run(makeRequest());
+                System.out.println(response);
 
-            stopWatch.stop();
+                stopWatch.stop();
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
         }
+
     }
 
     private String makeRequest() {
