a76b0ac2d53c8732888c7f075df0cc818022ac32
Maxim Yunusov <maximyunusov@gmail.com>
2014-09-14 16:16:43 +0700

    Горизонтальное масштабирование и маршрутизация

--- src/main/java/org/maxur/akkacluster/Repository.java
+++ src/main/java/org/maxur/akkacluster/Repository.java
@@ -1,10 +1,15 @@
 package org.maxur.akkacluster;
 
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import static java.lang.String.format;
 
 /**
  * @author Maxim Yunusov
@@ -14,13 +19,38 @@ public class Repository extends UntypedActor {
 
     private LoggingAdapter logger = Logging.getLogger(context().system(), this);
 
+    private final String port;
+
     private ActorRef sender;
+
+    public static void main(String[] args) throws Exception {
+        final String[] ports = args.length == 0 ?
+                new String[]{"2561", "2562"} :
+                args;
+        startup(ports);
+    }
+
+
+    private static void startup(String[] ports) {
+        for (String port : ports) {
+            final Config config = ConfigFactory
+                    .parseString("akka.remote.netty.tcp.port=" + port)
+                    .withFallback(ConfigFactory.load().getConfig("repository"));
+
+            ActorSystem system = ActorSystem.create("RepositorySystem", config);
+            system.actorOf(Props.create(Repository.class, port), "repository");
+        }
+    }
+
+    public Repository(String port) {
+        this.port = port;
+    }
 
     @Override
     public void onReceive(Object message) throws Exception {
         if (message instanceof String) {
             save(message.toString());
-            sender.tell(message, sender());
+            sender.tell(format("%s (%s)", message, port), sender());
         }
     }
 
