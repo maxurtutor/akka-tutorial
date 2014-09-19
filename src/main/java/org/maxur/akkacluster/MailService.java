584136652733fdc8d3c692efb7b0e98384ec0709
myunusov <myunusov@luxoft.com>
2014-09-19 12:39:59 +0700

    Оптимизируем производительность (декомпозиция)

--- src/main/java/org/maxur/akkacluster/MailService.java
+++ src/main/java/org/maxur/akkacluster/MailService.java
@@ -1,17 +1,39 @@
 package org.maxur.akkacluster;
 
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import static akka.actor.ActorRef.noSender;
+
 /**
  * @author Maxim Yunusov
  * @version 1.0 14.09.2014
  */
-public class MailService {
+public class MailService extends UntypedActor {
 
-    public void send(String response) throws InterruptedException {
-        // Имитация бурной деятельности
-        Thread.sleep(100);
+    private LoggingAdapter logger = Logging.getLogger(context().system(), this);
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof String) {
+            send(message.toString());
+            sender().tell(message, noSender());
+        }
     }
 
-    public void done() {
-        // TODO
+    @Override
+    public void preStart() throws Exception {
+        logger.info("Start Sender");
+    }
+
+    @Override
+    public void postStop() throws Exception {
+        logger.info("Stop Sender");
+    }
+
+    private  void send(String response) throws InterruptedException {
+        // Имитация бурной деятельности
+        Thread.sleep(100);
     }
 }
--- src/main/java/org/maxur/akkacluster/Repository.java
+++ src/main/java/org/maxur/akkacluster/Repository.java
@@ -1,17 +1,42 @@
 package org.maxur.akkacluster;
 
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
 /**
  * @author Maxim Yunusov
  * @version 1.0 14.09.2014
  */
-public class Repository {
+public class Repository extends UntypedActor {
 
-    public void save(String response) throws InterruptedException {
-        // Имитация бурной деятельности
-        Thread.sleep(100);
+    private LoggingAdapter logger = Logging.getLogger(context().system(), this);
+
+    private ActorRef sender;
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof String) {
+            save(message.toString());
+            sender.tell(message, sender());
+        }
     }
 
-    public void done() {
-        // TODO
+    @Override
+    public void preStart() throws Exception {
+        logger.info("Start Repository");
+        sender = context().actorOf(Props.create(MailService.class));
+    }
+
+    @Override
+    public void postStop() throws Exception {
+        logger.info("Stop Repository");
+    }
+
+    private void save(String response) throws InterruptedException {
+        // Имитация бурной деятельности
+        Thread.sleep(100);
     }
 }
