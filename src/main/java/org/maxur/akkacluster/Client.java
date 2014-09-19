552ee40038bca36b878182864bd4fe05513498d9
myunusov <myunusov@luxoft.com>
2014-09-19 12:31:11 +0700

    Снимаем проблему конкурентного доступа (используем Akka)

--- src/main/java/org/maxur/akkacluster/Client.java
+++ src/main/java/org/maxur/akkacluster/Client.java
@@ -1,56 +1,70 @@
 package org.maxur.akkacluster;
 
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.PoisonPill;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 import org.perf4j.log4j.Log4JStopWatch;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import static akka.actor.ActorRef.noSender;
 
 /**
  * @author Maxim Yunusov
  * @version 1.0 14.09.2014
  */
-public class Client implements Runnable {
+public class Client extends UntypedActor {
 
-    Logger logger = LoggerFactory.getLogger(Client.class);
+    private LoggingAdapter logger = Logging.getLogger(context().system(), this);
 
-
-    private Worker worker;
+    private ActorRef worker;
 
     public static void main(String[] args) throws Exception {
-        final Client client = new Client();
-        client.init();
-        new Thread(client).start();
-        new Thread(client).start();
-        client.done();
+        startSystem();
     }
 
-    private void init() {
-        worker = new Worker();
+    private static void startSystem() {
+        ActorSystem system = ActorSystem.create("ClientSystem");
+        system.actorOf(Props.create(Client.class));
     }
 
-    public void run() {
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof String) {
+            Log4JStopWatch stopWatch = new Log4JStopWatch("Client");
+            System.out.println(message);
+            stopWatch.stop();
+        }
+    }
+
+    @Override
+    public void preStart() {
+        logger.info("Start Client");
+        worker = context().system().actorOf(Props.create(Worker.class), "worker");
+        run();
+    }
+
+    @Override
+    public void postStop() {
+        worker.tell(PoisonPill.getInstance(), noSender());
+        context().system().shutdown();
+        logger.info("Stop Client");
+    }
+
+    private void run() {
         try {
             for (int i = 0; i < 100; i++) {
-                Log4JStopWatch stopWatch = new Log4JStopWatch("Client");
-
-                String response = worker.run(makeRequest());
-                System.out.println(response);
-
-                stopWatch.stop();
+                worker.tell(makeRequest(), self());
             }
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
-
     }
 
     private String makeRequest() {
         return "Hello";
     }
-
-    private void done() {
-        worker.done();
-    }
-
-
 }
